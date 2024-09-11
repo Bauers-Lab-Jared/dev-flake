e75a6013db9a2133a28646fc7029fc2f5e34b329
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-09-11 16:07:40 -0400

    updated to latest from odin-sudoku project

--- default.nix
+++ default.nix
@@ -1,37 +1,47 @@
 {
   stdenv,
   odin,
-  clang,
-  llvm_17,
   go-task,
   gdb,
-  # Args for this func will be sourced by nixpkgs automatically
+  coreutils,
+  #libGL, --use these for raylib
+  #xorg,
+  lib,
+  writeShellScript,
+  patchelf,
 }:
 stdenv.mkDerivation rec {
   pname = "projectName";
   version = "0.1";
-  src = ./src;
 
   # Inputs to be available at build time
+  # Now mostly for loading the dev environment.
+  # Our custom builder is overriding most of
+  # the automatic functionality of mkDerivation.
   nativeBuildInputs = [
     gdb
     go-task
     odin
-    clang
-    llvm_17
   ];
+  src = ./src/main;
 
   # Inputs to be available at runtime
   buildInputs = [
+    #libGL -- use these for raylib
+    #xorg.libX11
   ];
 
-  buildPhase = ''
-    odin build ./main/ \
-    -out:${pname}
-  '';
+  builder = let
+    libPath = lib.makeLibraryPath buildInputs;
+  in
+    writeShellScript "builder.sh" ''
+      export PATH="${coreutils}/bin:${odin}/bin"
+      mkdir -p $out/bin
+      odin build $src -out:$out/bin/$pname
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ${pname} $out/bin
-  '';
+      ${patchelf}/bin/patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        --set-rpath "${libPath}" \
+        $out/bin/${pname}
+    '';
 }
--- flake.nix
+++ flake.nix
@@ -19,11 +19,18 @@
       packages.default = appliedOverlay.default;
       devShells.default = pkgs.mkShell {
         inherit (appliedOverlay.default) nativeBuildInputs buildInputs;
+        LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
+          pkgs.lib.makeLibraryPath appliedOverlay.default.buildInputs
+        }";
 
         packages = [
           (self.inputs.nixvim.lib.mkNixvim {
             pkgs = nixvimPkgs;
-            addons = ["proj-odin" "proj-nix"];
+            addons = [
+              #"proj-html" use this for web projects
+              "proj-odin"
+              "proj-nix"
+            ];
           })
         ];
       };
