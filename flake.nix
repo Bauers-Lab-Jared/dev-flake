8e48684ccf92187e40a1d73ba76e9936e7df43c8
wafip <wafip@sgtwaffle.com>
2025-08-11 10:31:54 -0400

    updated environment and nixvim source

--- flake.nix
+++ flake.nix
@@ -1,56 +1,57 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixvim.url = "github:bauers-lab-jared/nixvim";
     roc.url = "github:roc-lang/roc";
+    nixvim-waffle.url = "git+ssh://gitea@git.sgtwaffle.com:2022/waffle/nixvim?ref=main";
   };
 
-  outputs = {
-    self,
-    roc,
-    flake-utils,
-    ...
-  }: let
-    out = system: let
-      pkgs =
-        import roc.inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        }
-        // {rocpkgs = roc.packages.${system};};
-      nixvimPkgs = self.inputs.nixvim.inputs.nixpkgs.legacyPackages.${system};
-      appliedOverlay = self.overlays.default pkgs pkgs;
-      rocShell = roc.devShell.${system};
-    in {
-      packages.default = appliedOverlay.default;
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [rocShell];
-        inherit (appliedOverlay.default) nativeBuildInputs buildInputs;
+  outputs =
+    {
+      self,
+      roc,
+      flake-utils,
+      ...
+    }:
+    let
+      user = "waffle";
+      out =
+        system:
+        let
+          pkgs =
+            import roc.inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }
+            // {
+              rocpkgs = roc.packages.${system};
+            };
+          appliedOverlay = self.overlays.default pkgs pkgs;
+          rocShell = roc.devShells.${system}.default;
+        in
+        {
+          packages.default = appliedOverlay.default;
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ rocShell ];
+            inherit (appliedOverlay.default) nativeBuildInputs buildInputs;
 
-        inherit
-          (rocShell)
-          NIX_GLIBC_PATH # env vars
-          LD_LIBRARY_PATH
-          NIXPKGS_ALLOW_UNFREE
-          shellHook # to set the LLVM_SYS_<VERSION>_PREFIX
-          ;
+            inherit (rocShell)
+              NIX_GLIBC_PATH # env vars
+              LD_LIBRARY_PATH
+              shellHook # to set the LLVM_SYS_<VERSION>_PREFIX
+              ;
 
-        packages = [
-          (self.inputs.nixvim.lib.mkNixvim {
-            pkgs = nixvimPkgs;
-            # Add nixmodules below
-            addons = [
-              "proj-nix"
+            packages = [
+              (self.inputs."nixvim-${user}".nixvimWithFlags.${system} [
+                "roc"
+              ])
             ];
-          })
-        ];
-      };
-    };
-  in
+          };
+        };
+    in
     flake-utils.lib.eachDefaultSystem out
     // {
       overlays.default = final: prev: {
-        default = final.callPackage ./default.nix {inherit (prev) rocpkgs;};
+        default = final.callPackage ./default.nix { inherit (prev) rocpkgs; };
       };
     };
 }
--- src/main.roc
+++ src/main.roc
@@ -1,6 +1,11 @@
-app [main] { pf: platform "https://github.com/roc-lang/basic-cli/releases/download/0.15.0/SlwdbJ-3GR7uBWQo6zlmYWNYOxnvo8r6YABXD-45UOw.tar.br" }
+app [main!] {
+    cli: platform "https://github.com/roc-lang/basic-cli/releases/download/0.19.0/Hj-J_zxz7V9YurCSTFcFdu6cQJie4guzsPMUi5kBYUk.tar.br",
+    ascii: "https://github.com/Hasnep/roc-ascii/releases/download/v0.3.1/1PCTQ0tzSijxfhxDg1k_yPtfOXiAk3j283b8EWGusVc.tar.br",
+}
 
-import pf.Stdout
+import cli.Stdout
+import cli.Arg exposing [Arg]
 
-main =
+  main! : List Arg => Result {} _
+  main! = |_args|
     Stdout.line! "I'm a Roc application!"
